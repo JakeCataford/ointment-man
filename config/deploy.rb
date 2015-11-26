6218ba05127476d999a42e8bf22dd8a89de9d34f
Jake Cataford <jakecataford@gmail.com>
2015-11-26 20:05:57 +0000

    beh

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,10 +2,6 @@
 lock '3.4.0'
 set :application, 'ointment-man'
 set :repo_url, 'git@github.com:JakeCataford/ointment-man'
-
-require "capistrano/rvm"
-
 set :rvm_ruby_string, :local        # use the same ruby as used locally for deployment
-
 before 'deploy', 'rvm:install_rvm'  # install/update RVM
 before 'deploy', 'rvm:install_ruby' # install Ruby and create gemset (both if missing)
