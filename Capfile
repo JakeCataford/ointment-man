4b749a474d97bc16dfba7270139313e60b1463dc
Jake Cataford <jakecataford@gmail.com>
2015-11-26 20:02:55 +0000

    sszs

--- Capfile
+++ Capfile
@@ -1,5 +1,4 @@
 require 'capistrano/setup'
-require 'capistrano/rvm'
 require 'capistrano/deploy'
 require 'capistrano/unicorn_nginx'
 Dir.glob('lib/capistrano/tasks/*.rake').each { |r| import r }
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,11 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
-
 set :application, 'ointment-man'
 set :repo_url, 'git@github.com:JakeCataford/ointment-man'
+
+require "rvm/capistrano"
+
+set :rvm_ruby_string, :local        # use the same ruby as used locally for deployment
+
+before 'deploy', 'rvm:install_rvm'  # install/update RVM
+before 'deploy', 'rvm:install_ruby' # install Ruby and create gemset (both if missing)
